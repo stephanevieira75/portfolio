7faa44dfeb0b4e2860ca62a2eae557c4cf3e5b34
stephanevieira75 <stephane.vieira93@outlook.fr>
2018-02-19 10:41:13 +0100

    modification about form, and links

--- contact.php
+++ contact.php
@@ -1,9 +1,9 @@
-<?php
 
+<?php
 //Retrieve form data. 
 //GET - user submitted data using AJAX
 //POST - in case user does not support javascript, we'll use POST instead
-$name = ($_GET['name']) ? $_GET['name'] : $_POST['name'];
+$name = ($_GET['name']) ?$_GET['name'] : $_POST['name'];
 $email = ($_GET['email']) ?$_GET['email'] : $_POST['email'];
 $comment = ($_GET['comment']) ?$_GET['comment'] : $_POST['comment'];
 
@@ -12,9 +12,9 @@ $comment = ($_GET['comment']) ?$_GET['comment'] : $_POST['comment'];
 if ($_POST) $post=1;
 
 //Simple server side validation for POST data, of course, you should validate the email
-if (!$name) $errors[count($errors)] = 'Please enter your name.';
-if (!$email) $errors[count($errors)] = 'Please enter your email.'; 
-if (!$comment) $errors[count($errors)] = 'Please enter your message.'; 
+if (!$name) $errors[count($errors)] = 'Veuillez inserer un nom valide.';
+if (!$email) $errors[count($errors)] = 'Veuillez inserer un email valide.'; 
+if (!$comment) $errors[count($errors)] = 'Veuillez écrire un message.'; 
 
 //if the errors array is empty, send the mail
 if (!$errors) {
@@ -25,8 +25,8 @@ if (!$errors) {
 	$from = $name . ' <' . $email . '>';
 	
 	//subject and the html message
-	$subject = 'Message via Aries from ' . $name;	
-	$message = 'Name: ' . $name . '<br/><br/>
+	$subject = 'Message via mon Portfolio de ' . $name;	
+	$message = 'Nom/prénom: ' . $name . '<br/><br/>
 		       Email: ' . $email . '<br/><br/>		
 		       Message: ' . nl2br($comment) . '<br/>';
 
@@ -35,8 +35,8 @@ if (!$errors) {
 	
 	//if POST was used, display the message straight away
 	if ($_POST) {
-		if ($result) echo 'Thank you! We have received your message.';
-		else echo 'Sorry, unexpected error. Please try again later';
+		if ($result) echo 'Merci! Nous avons bien recu ton message!.';
+		else echo 'Désolé, une erreur est survenue. Recommence!';
 		
 	//else if GET was used, return the boolean value so that 
 	//ajax script can react accordingly
@@ -49,12 +49,12 @@ if (!$errors) {
 } else {
 	//display the errors message
 	for ($i=0; $i<count($errors); $i++) echo $errors[$i] . '<br/>';
-	echo '<a href="index.html">Back</a>';
+	echo '<a href="index.html">Retour</a>';
 	exit;
 }
 
 
-//Simple mail function with HTML header
+//mail function with HTML header
 function sendmail($to, $subject, $message, $from) {
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
@@ -67,3 +67,4 @@ function sendmail($to, $subject, $message, $from) {
 }
 
 ?>
+
